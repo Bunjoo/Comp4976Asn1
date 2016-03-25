ba7eff25f428ec285963715ffdb0b23834820b39
Aicha Hezit <aichez18@hotmail.com>
2016-03-24 19:38:53 -0700

    User/Role management complete, Choice data seeded

---
+++ DiplomaOptions/OptionsWebSite/Migrations/DiplomaOptionsMigrations/201603250233269_Second Choice Seed.cs
@@ -0,0 +1,16 @@
+namespace OptionsWebSite.Migrations.DiplomaOptionsMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SecondChoiceSeed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
